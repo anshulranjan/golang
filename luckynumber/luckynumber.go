51356038f812e652fd8babf42444b55f1a9e8ad5
anshulranjan <ranjananshul1@gmail.com>
2022-02-01 20:24:53 +0530

    LuckyNumber

---
+++ luckynumber/luckynumber.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+func input(a int) int {
+	var input string
+	fmt.Printf("Let's guess the number. Attempt %d", a)
+	fmt.Println()
+	fmt.Scanln(&input)
+	number, err := strconv.Atoi(input)
+	if err != nil {
+		return 0
+	}
+	return number
+}
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	givennumber := input(1)
+	guess := 10
+	if givennumber == 0 {
+		fmt.Println("Please choose a postive number")
+		return
+	}
+	turn := 0
+	for ; turn < 4; turn++ {
+		n := rand.Intn(guess + 1)
+		if n == guess {
+			if turn == 0 {
+				fmt.Println("Congrats!!! Since you guess in first attempt you won special prize")
+			} else {
+				fmt.Println("Congrats!!! You Won")
+			}
+			break
+		} else {
+			givennumber = input(turn + 2)
+		}
+	}
+	if turn == 5 {
+		fmt.Println("You lost. Try Again")
+	}
+
+}
