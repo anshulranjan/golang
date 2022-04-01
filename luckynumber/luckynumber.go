600c5fdd05110243e6cb1dd4acd059846304f618
anshulranjan <ranjananshul1@gmail.com>
2022-04-01 17:26:19 +0530

    LuckyNumber

--- luckynumber/luckynumber.go
+++ luckynumber/luckynumber.go
@@ -16,23 +16,29 @@ func input(a int) int {
 	fmt.Println()
 	fmt.Scanln(&input)
 	number, err := strconv.Atoi(input)
+	if number < 0 && number > 10 {
+		fmt.Println("The number should be between 0 and 10")
+		return -1
+	}
 	if err != nil {
-		return 0
+		return -1
 	}
 	return number
 }
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	givennumber := input(1)
-	guess := 10
-	if givennumber < 0 {
-		fmt.Println("Please choose a postive number")
-		return
-	}
+	guess := rand.Intn(10)
+	fmt.Println("Welcome to the game")
+	fmt.Println("You have to guess the right number")
+	fmt.Println("The number will be between 0 and 10")
+	fmt.Println("You will have 3 attempt to guess the number")
 	turn := 0
-	for ; turn < 4; turn++ {
-		n := rand.Intn(guess + 1)
-		if n == givennumber {
+	for ; turn < 3; turn++ {
+		outputnumber := input(turn + 1)
+		if outputnumber == -1 {
+			continue
+		}
+		if guess == outputnumber {
 			if turn == 0 {
 				fmt.Println("Congrats!!! Since you guess in first attempt you won special prize")
 			} else {
@@ -40,10 +46,10 @@ func main() {
 			}
 			break
 		} else {
-			givennumber = input(turn + 2)
+			guess = rand.Intn(10)
 		}
 	}
-	if turn == 4 {
+	if turn == 3 {
 		fmt.Println("Sorry You lost. Try Again")
 	}
 }
